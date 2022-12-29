8951a3ede21bd60da7591555ab8b980add87b4d3
Jake Hamilton <jake.hamilton@hey.com>
2022-12-28 22:16:35 -0800

    fix: remove specified cows from overlay

--- overlays/cowfiles/default.nix
+++ overlays/cowfiles/default.nix
@@ -1,5 +1,25 @@
-{ channels, cowfiles, ... }:
+{ channels
+, cowfiles
+, ...
+}:
 
-final: prev: {
-  inherit cowfiles;
+final: prev:
+let
+  blocklist = [
+    "pepe"
+  ];
+  filtered-cowfiles = prev.runCommandNoCC "filtered-cowfiles" { src = cowfiles; } ''
+    blocked_files=(${builtins.foldl' (acc: name: "${acc} \"${name}.cow\"") "" blocklist})
+
+    mkdir -p $out/cows
+
+    for f in $src/cows/*.cow; do
+      if [ -f "$f" ] && [[ ! "''${blocked_files[@]}" =~ "$(basename $f)" ]]; then
+        ln -s $f "$out/cows/$(basename $f)"
+      fi
+    done
+  '';
+in
+{
+  cowfiles = filtered-cowfiles;
 }
