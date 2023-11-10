d2560890ab52a5a38022c34fd362287aab362db4
Jake Hamilton <jake.hamilton@hey.com>
2023-11-09 16:45:39 -0800

    feat: upgrade nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,16 +2,18 @@
   description = "Snowfall Cowsay";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # NOTE: ttyd had a bug on 23.05 that caused whitespace characters
+    # to not receive colors which broke cow2img. Currently, unstable
+    # is required in order to function properly.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     snowfall-lib = {
-      url = "github:snowfallorg/lib";
+      url = "github:snowfallorg/lib?ref=v2.1.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     vhs = {
-      url = "github:snowfallorg/vhs";
+      url = "github:snowfallorg/vhs?ref=v1.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -22,33 +24,8 @@
   };
 
   outputs = inputs:
-    let
-      inherit (inputs.nixpkgs) lib;
-      inherit (lib) mapAttrsToList flatten foldl pipe;
-
-      collect-packages =
-        (system: packages:
-          mapAttrsToList
-            (name: package: {
-              inherit system name package;
-            })
-            packages
-        );
-
-      collected-packages = flatten (
-        mapAttrsToList collect-packages inputs.self.packages
-      );
-
-      create-jobs = jobs: entry: jobs // {
-        ${entry.name} = (jobs.${entry.name} or { }) // {
-          ${entry.system} = entry.package;
-        };
-      };
-
-      hydraJobs = foldl create-jobs { } collected-packages;
-    in
     inputs.snowfall-lib.mkFlake {
-      inherit inputs hydraJobs;
+      inherit inputs;
 
       src = ./.;
 
